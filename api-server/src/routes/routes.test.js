e7e4af51b84c142beb66d6f22da0e976d1e04510
viralpatel5757 <viralpatel5757@gmail.com>
2022-04-22 21:59:01 +0000

    test update

--- api-server/src/routes/routes.test.js
+++ api-server/src/routes/routes.test.js
@@ -41,7 +41,7 @@ describe('Test routes', () => {
                 .end((err, res) => {
                     res.should.have.status((200));
                     res.body.should.be.a('object');
-                    res.body.config.team.should.a('string');
+                    res.body.config.team.should.be.a('string');
                     done();
                 })
         });
